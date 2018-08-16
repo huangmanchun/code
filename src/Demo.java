835ad6f38bb6e0c4ba82466a37189ba3f2182f07
huangmanchun <mc0701@126.com>
2018-08-16 17:43:48 +0800

    initial commit

---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+    public static void main(String[] agrs){
+        String str = "hello";
+        System.out.println(str);
+        show();
+    }
+
+    public static void show(){
+        System.out.println("111111111111");
+    }
+}
